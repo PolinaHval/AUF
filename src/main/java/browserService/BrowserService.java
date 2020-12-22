3d8036865d8dd10fd700e4f48f015eebd677a669
PolinaHval <liza.khval@mail.ru>
2020-12-22 23:56:23 +0300

    commit

--- src/main/java/browserService/BrowserService.java
+++ src/main/java/browserService/BrowserService.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-import java.io.File;
+
 
 public class BrowserService {
     private WebDriver driver = null;
